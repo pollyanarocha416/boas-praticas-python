e6027cecc4a23c8a48a7ede7d0f497fe53e34042
pollyana <pollyana.rocha416@gmail.com>
2025-07-17 17:51:37 -0400

    adicionando docstring

--- app/routers/routers_produtos.py
+++ app/routers/routers_produtos.py
@@ -13,6 +13,15 @@ historico_compras: Dict[int, List[int]] = {}
 
 @router.post("/produtos/", response_model=Produto)
 def criar_produto(produto: CriarProduto) -> Produto:
+    """
+    Cria um novo produto.
+
+    Args:
+        produto (CriarProduto): O objeto contendo os dados do produto a ser criado.
+
+    Returns:
+        Produto: O objeto do produto recém-criado com um ID gerado.
+    """
     global contador_produto
     novo_produto = Produto(id=contador_produto, **produto.model_dump())
     produtos.append(novo_produto)
@@ -22,6 +31,12 @@ def criar_produto(produto: CriarProduto) -> Produto:
 
 @router.get("/produtos/", response_model=List[Produto])
 def listar_produtos():
+    """
+    Lista todos os produtos cadastrados.
+
+    Returns:
+        List[Produto]: Uma lista de objetos de produtos cadastrados.
+    """
     return produtos
 
 
@@ -29,6 +44,16 @@ def listar_produtos():
 def adicionar_historico_compras(
     usuario_id: int, compras: HistoricoCompras
 ) -> Dict[str, str]:
+    """
+    Adiciona ou atualiza o histórico de compras de um usuário.
+
+    Args:
+        usuario_id (int): O ID do usuário para o qual o histórico de compras será atualizado.
+        compras (HistoricoCompras): O objeto contendo os IDs dos produtos comprados.
+
+    Returns:
+        dict: Mensagem indicando que o histórico de compras foi atualizado.
+    """
     if usuario_id not in [usuario.id for usuario in usuarios]:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     historico_compras[usuario_id] = compras.produtos_ids
@@ -37,6 +62,19 @@ def adicionar_historico_compras(
 
 @router.post("/recomendacoes/{usuario_id}", response_model=List[Produto])
 def recomendar_produtos(usuario_id: int, preferencias: Preferencias) -> List[Produto]:
+    """
+    Recomenda produtos com base no histórico de compras e preferências do usuário.
+
+    Args:
+        usuario_id (int): O ID do usuário para o qual as recomendações serão feitas.
+        preferencias (Preferencias): O objeto contendo as preferências do usuário, como categorias e tags.
+
+    Raises:
+        HTTPException: Se o histórico de compras não for encontrado para o usuário.
+
+    Returns:
+        List[Produto]: Uma lista de produtos recomendados com base no histórico de compras e preferências.
+    """
     if usuario_id not in historico_compras:
         raise HTTPException(
             status_code=404, detail="Histórico de compras não encontrado"
--- app/routers/routers_usuarios.py
+++ app/routers/routers_usuarios.py
@@ -10,6 +10,15 @@ contador_usuario: int = 1
 
 @router.post("/usuarios/", response_model=Usuario)
 def criar_usuario(nome: str) -> Usuario:
+    """
+    Rota para cadastrar novos usuários.
+
+    Args:
+        nome (str): nome do usuário a ser criado.
+
+    Returns:
+        Usuario: O objeto do usuário criado com um ID gerado.
+    """
     global contador_usuario
     novo_usuario = Usuario(id=contador_usuario, nome=nome)
     usuarios.append(novo_usuario)
@@ -19,4 +28,10 @@ def criar_usuario(nome: str) -> Usuario:
 
 @router.get("/usuarios/", response_model=List[Usuario])
 def listar_usuarios() -> List[Usuario]:
+    """
+    Lista todos os usuários cadastrados.
+
+    Returns:
+        List[Usuario]: Uma lista de objetos de usuários cadastrados.
+    """
     return usuarios
