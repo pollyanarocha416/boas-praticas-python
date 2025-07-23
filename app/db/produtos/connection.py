b8314dccbbdff62d29cf87309dbba0eefb23f94c
pollyana <pollyana.rocha416@gmail.com>
2025-07-23 09:51:26 -0400

    corrigindo import, maneira de importar depreciada

--- app/db/produtos/connection.py
+++ app/db/produtos/connection.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 
 engine = create_engine("mysql://root:1234@localhost:3306/produtos")
